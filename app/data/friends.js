48c4e83feb48335eeded40facbdf9673807bf44e
mohamed shire <m.x.shire876@hotmail.com>
2019-04-23 03:03:41 -0500

    i modified th README filr

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,165 +1,165 @@
 
 var friends = [
     {
-      "name": "Ahmed",
-      "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-      "scores": [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
     },
     {
-      "name": "Jacob Deming",
-      "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-      "scores": [
-        "4",
-        "2",
-        "5",
-        "1",
-        "3",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2"
-      ]
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
     },
     {
-      "name": "Jeremiah Scanlon",
-      "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-      "scores": [
-        "5",
-        "2",
-        "2",
-        "2",
-        "4",
-        "1",
-        "3",
-        "2",
-        "5",
-        "5"
-      ]
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
     },
     {
-      "name": "Louis T. Delia",
-      "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-      "scores": [
-        "3",
-        "3",
-        "4",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2",
-        "2",
-        "3"
-      ]
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
     },
     {
-      "name": "Lou Ritter",
-      "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-      "scores": [
-        "4",
-        "3",
-        "4",
-        "1",
-        "5",
-        "2",
-        "5",
-        "3",
-        "1",
-        "4"
-      ]
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
     },
     {
-      "name": "Jordan Biason",
-      "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-      "scores": [
-        "4",
-        "4",
-        "2",
-        "3",
-        "2",
-        "2",
-        "3",
-        "2",
-        "4",
-        "5"
-      ]
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
     },
     {
-      "name": "Rhardin94",
-      "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiZxpLZlOThAhVNiOAKHSJcDzoQjRx6BAgBEAU&url=https%3A%2F%2Funsplash.com%2Fphotos%2F5EIW3DDX6dw&psig=AOvVaw1DkLIe5EcvrATKwqs1lgoS&ust=1556038345224893",
-      "scores": [
-        "5",
-        "5",
-        "4",
-        "3",
-        "5",
-        "2",
-        "2",
-        "1",
-        "3",
-        "1"
-      ]
+        "name": "Rhardin94",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiZxpLZlOThAhVNiOAKHSJcDzoQjRx6BAgBEAU&url=https%3A%2F%2Funsplash.com%2Fphotos%2F5EIW3DDX6dw&psig=AOvVaw1DkLIe5EcvrATKwqs1lgoS&ust=1556038345224893",
+        "scores": [
+            "5",
+            "5",
+            "4",
+            "3",
+            "5",
+            "2",
+            "2",
+            "1",
+            "3",
+            "1"
+        ]
     },
     {
-      "name": "coco",
-      "photo": "https://unsplash.com/photos/n5aE6hOY6do",
-      "scores": [
-        "5",
-        "4",
-        "3",
-        "1",
-        "5",
-        "4",
-        "5",
-        "2",
-        "2",
-        "4"
-      ]
+        "name": "coco",
+        "photo": "https://unsplash.com/photos/n5aE6hOY6do",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "1",
+            "5",
+            "4",
+            "5",
+            "2",
+            "2",
+            "4"
+        ]
     },
     {
-      "name": "blahblah",
-      "photo": "jdkidockmdene",
-      "scores": [
-        "3",
-        "1",
-        "1",
-        "1",
-        "1",
-        "2",
-        "2",
-        "2",
-        "3",
-        "3"
-      ]
+        "name": "blahblah",
+        "photo": "jdkidockmdene",
+        "scores": [
+            "3",
+            "1",
+            "1",
+            "1",
+            "1",
+            "2",
+            "2",
+            "2",
+            "3",
+            "3"
+        ]
     },
     {
-      "name": "Testing",
-      "photo": "link",
-      "scores": [
-        "3",
-        "2",
-        "3",
-        "2",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2",
-        "1"
-      ]
+        "name": "Testing",
+        "photo": "link",
+        "scores": [
+            "3",
+            "2",
+            "3",
+            "2",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1"
+        ]
     },
-    ]
+]
 
-    module.exports = friends;
+module.exports = friends;
